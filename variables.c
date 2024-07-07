fd65127abeaf2b919b764fa056fc8b2295c26250
Mohamed Hammou <hammou.mohamned@gmail.com>
2024-07-07 14:59:51 -0500

    The file has been formated

--- variables.c
+++ variables.c
@@ -3,15 +3,19 @@
 
 int main() {
   int num1, num2, sum, def, prod;
+
   printf("Enter the first number : ");
   scanf("%d", &num1);
   printf("Enter the second number : ");
   scanf("%d", &num2);
+
   sum = num1 + num2;
   def = num1 - num2;
   prod = num1*num2;
+
   printf("sum(%d,%d) = %d\n", num1, num2, sum);
   printf("def(%d,%d) = %d\n", num1, num2, def);
   printf("prod(%d,%d) = %d\n", num1, num2, prod);
+  
   return 0;
 }
