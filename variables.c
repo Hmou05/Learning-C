671cd315577d50bca71e076490ccc929cbd0e837
Mohamed Hammou <hammou.mohamned@gmail.com>
2024-07-08 12:42:28 -0500

    Apply some string built-in methods and casting, gets() and puts()

--- variables.c
+++ variables.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-  int num1, num2, sum, def, prod;
-
+  int num1, num2;
+  char x[] = "Mohamed";
   printf("Enter the first number : ");
   scanf("%d", &num1);
   printf("Enter the second number : ");
   scanf("%d", &num2);
 
-  sum = num1 + num2;
-  def = num1 - num2;
-  prod = num1*num2;
+  printf("sum(%d,%d) = %d\n", num1, num2,num1 + num2);
+  printf("def(%d,%d) = %d\n", num1, num2,num1 - num2);
+  printf("prod(%d,%d) = %d\n", num1, num2, num1*num2);
+  printf("dev(%d,%d) = %lf\n", num1, num2, (double)num1/num2);
+  strcat(x, " ");
+  printf("The string is '%s'", x);
 
-  printf("sum(%d,%d) = %d\n", num1, num2, sum);
-  printf("def(%d,%d) = %d\n", num1, num2, def);
-  printf("prod(%d,%d) = %d\n", num1, num2, prod);
-  
   return 0;
 }
